1dced65770d03cac8f59087373873053a67f136d
Mariana Samardzic <marianasamardzic2@gmail.com>
2022-10-24 17:08:49 +0200

    Calculate arrival rate

--- fltk/util/statistics/arrival_rate_estimator.py
+++ fltk/util/statistics/arrival_rate_estimator.py
@@ -1,4 +1,5 @@
 import abc
+from datetime import datetime
 from typing import List
 
 
@@ -12,16 +13,27 @@ class Estimator:
 class ArrivalRateEstimator(Estimator):
     job_counter: int = 0
     job_execution_times: List[float] = list()
+    previous_arrival_timestamp = None
+    inter_arrival_sum = 0
 
     def __init__(self):
         super().__init__()
         pass
 
     def new_arrival(self):
-        self.job_counter += 1
+        new_timestamp = datetime.now()
+        if self.previous_arrival_timestamp is not None:
+            self.job_counter += 1
+            inter_arrival_time = new_timestamp - self.previous_arrival_timestamp
+            self.inter_arrival_sum += inter_arrival_time.total_seconds()
+        self.previous_arrival_timestamp = new_timestamp
 
     def new_job_finish(self, job_execution_time):
         self.job_execution_times.append(job_execution_time)
 
     def estimate_arrival_rate(self) -> float:
-        return 2.5  # 3 nodes
+        if self.job_counter != 0:
+            mean_inter_arrival_time = self.inter_arrival_sum / self.job_counter
+            return 1 / mean_inter_arrival_time
+        else:
+            return None
--- tests/statistics/arrival_rate_estimator.py
+++ tests/statistics/arrival_rate_estimator.py
@@ -1,3 +1,4 @@
+import time
 import unittest
 
 from fltk.util.statistics.arrival_rate_estimator import ArrivalRateEstimator
@@ -8,5 +9,30 @@ class ArrivalRateEstimatorTest(unittest.TestCase):
     def setUp(self):
         self.estimator = ArrivalRateEstimator()
 
-    def test_example(self):
-        self.assertEquals(2, 2)
+    def test_2_arrivals(self):
+        self.estimator.new_arrival()
+        time.sleep(2)
+        self.estimator.new_arrival()
+        self.assertEquals(self.estimator.job_counter, 1)
+        self.assertAlmostEqual(self.estimator.inter_arrival_sum, 2, 1)
+        self.assertAlmostEqual(self.estimator.estimate_arrival_rate(), 0.5, 1)
+
+    def test_3_arrivals(self):
+        self.estimator.new_arrival()
+        time.sleep(2)
+        self.estimator.new_arrival()
+        time.sleep(2)
+        self.estimator.new_arrival()
+        self.assertEquals(self.estimator.job_counter, 2)
+        self.assertAlmostEqual(self.estimator.inter_arrival_sum, 4, 1)
+        self.assertAlmostEqual(self.estimator.estimate_arrival_rate(), 0.5, 1)
+
+    def test_3_different_arrivals(self):
+        self.estimator.new_arrival()
+        time.sleep(10)
+        self.estimator.new_arrival()
+        time.sleep(2)
+        self.estimator.new_arrival()
+        self.assertEquals(self.estimator.job_counter, 2)
+        self.assertAlmostEqual(self.estimator.inter_arrival_sum, 12, 1)
+        self.assertAlmostEqual(self.estimator.estimate_arrival_rate(), 0.166, 1)
