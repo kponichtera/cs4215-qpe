c3ff590778d76fb09e2d7bff3f38c4699e309d2e
Mariana Samardzic <marianasamardzic2@gmail.com>
2022-10-24 17:17:05 +0200

    Added service rate estimation

--- fltk/util/statistics/arrival_rate_estimator.py
+++ fltk/util/statistics/arrival_rate_estimator.py
@@ -9,10 +9,15 @@ class Estimator:
     def estimate_arrival_rate(self) -> float:
         raise NotImplementedError()
 
+    @abc.abstractmethod
+    def estimate_service_rate(self) -> float:
+        raise NotImplementedError()
+
 
 class ArrivalRateEstimator(Estimator):
-    job_counter: int = 0
-    job_execution_times: List[float] = list()
+    inter_arrival_counter: int = 0
+    completed_jobs_counter = 0
+    service_time_sum = 0
     previous_arrival_timestamp = None
     inter_arrival_sum = 0
 
@@ -23,17 +28,23 @@ class ArrivalRateEstimator(Estimator):
     def new_arrival(self):
         new_timestamp = datetime.now()
         if self.previous_arrival_timestamp is not None:
-            self.job_counter += 1
+            self.inter_arrival_counter += 1
             inter_arrival_time = new_timestamp - self.previous_arrival_timestamp
             self.inter_arrival_sum += inter_arrival_time.total_seconds()
         self.previous_arrival_timestamp = new_timestamp
 
     def new_job_finish(self, job_execution_time):
-        self.job_execution_times.append(job_execution_time)
+        self.completed_jobs_counter += 1
+        self.service_time_sum += job_execution_time
 
     def estimate_arrival_rate(self) -> float:
-        if self.job_counter != 0:
-            mean_inter_arrival_time = self.inter_arrival_sum / self.job_counter
+        if self.inter_arrival_counter != 0:
+            mean_inter_arrival_time = self.inter_arrival_sum / self.inter_arrival_counter
             return 1 / mean_inter_arrival_time
         else:
             return None
+
+    def estimate_service_rate(self) -> float:
+        if self.completed_jobs_counter != 0:
+            mean_service_time = self.service_time_sum / self.completed_jobs_counter
+            return 1 / mean_service_time
--- tests/statistics/arrival_rate_estimator.py
+++ tests/statistics/arrival_rate_estimator.py
@@ -13,7 +13,7 @@ class ArrivalRateEstimatorTest(unittest.TestCase):
         self.estimator.new_arrival()
         time.sleep(2)
         self.estimator.new_arrival()
-        self.assertEquals(self.estimator.job_counter, 1)
+        self.assertEquals(self.estimator.inter_arrival_counter, 1)
         self.assertAlmostEqual(self.estimator.inter_arrival_sum, 2, 1)
         self.assertAlmostEqual(self.estimator.estimate_arrival_rate(), 0.5, 1)
 
@@ -23,7 +23,7 @@ class ArrivalRateEstimatorTest(unittest.TestCase):
         self.estimator.new_arrival()
         time.sleep(2)
         self.estimator.new_arrival()
-        self.assertEquals(self.estimator.job_counter, 2)
+        self.assertEquals(self.estimator.inter_arrival_counter, 2)
         self.assertAlmostEqual(self.estimator.inter_arrival_sum, 4, 1)
         self.assertAlmostEqual(self.estimator.estimate_arrival_rate(), 0.5, 1)
 
@@ -33,6 +33,20 @@ class ArrivalRateEstimatorTest(unittest.TestCase):
         self.estimator.new_arrival()
         time.sleep(2)
         self.estimator.new_arrival()
-        self.assertEquals(self.estimator.job_counter, 2)
+        self.assertEquals(self.estimator.inter_arrival_counter, 2)
         self.assertAlmostEqual(self.estimator.inter_arrival_sum, 12, 1)
         self.assertAlmostEqual(self.estimator.estimate_arrival_rate(), 0.166, 1)
+
+    def test_2_completed_jobs(self):
+        self.estimator.new_job_finish(2)
+        self.estimator.new_job_finish(2)
+        self.assertEquals(self.estimator.completed_jobs_counter, 2)
+        self.assertAlmostEqual(self.estimator.service_time_sum, 4, 1)
+        self.assertAlmostEqual(self.estimator.estimate_service_rate(), 0.5, 1)
+
+    def test_2_different_completed_jobs(self):
+        self.estimator.new_job_finish(2)
+        self.estimator.new_job_finish(16)
+        self.assertEquals(self.estimator.completed_jobs_counter, 2)
+        self.assertAlmostEqual(self.estimator.service_time_sum, 18, 1)
+        self.assertAlmostEqual(self.estimator.estimate_service_rate(), 1/9, 1)
