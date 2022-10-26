35df75326070bea33338e7f4bcf0c0e172a79051
Dan Plamadeala <cornul11@gmail.com>
2022-10-26 16:50:55 +0200

    fixed log filename and various minor changes

--- fltk/util/statistics/data_collector.py
+++ fltk/util/statistics/data_collector.py
@@ -4,22 +4,31 @@ from datetime import datetime
 
 
 class DataCollector:
+    _column_names = ['timestamp',
+                     'num_nodes',
+                     'current_utilisation',
+                     'total_utilisation',
+                     'estimated_nodes_num',
+                     'pending_tasks_count']
 
     def __init__(self):
         self._output_file = None
         self._logger = logging.getLogger('DataCollector')
         self.initialize_file()
+        self._header = ','.join(self._column_names)
+        self._starting_time = datetime.now().timestamp()
 
     def initialize_file(self):
         self._logger.info("Spinning up data collection mechanism...")
+
         now = datetime.now().isoformat()
-        file_name = f'{now}.log'
+        file_name = f'{now}.csv'
         self._output_file = open(file_name, 'w')
-        self._output_file.write('timestamp,num_nodes,current_utilisation,total_utilisation,estimated_nodes_num\n')
+        self._output_file.write(self._header + '\n')
 
     def log(self, num_nodes, current_utilisation, total_utilisation, estimated_nodes_num):
-        now = datetime.now().strftime("%H:%m:%S")
-        line = f'{now},{num_nodes},{current_utilisation},{total_utilisation},{estimated_nodes_num}\n'
+        timestamp = self._starting_time - datetime.now().timestamp()
+        line = f'{timestamp},{num_nodes},{current_utilisation},{total_utilisation},{estimated_nodes_num}\n'
         self.write_line(line)
 
     def write_line(self, line):
