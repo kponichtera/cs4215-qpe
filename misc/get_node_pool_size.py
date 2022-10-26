85f1026beae55ec0d7ee93f33a926b771fc497fc
Dan Plamadeala <cornul11@gmail.com>
2022-10-26 12:29:11 +0200

    local merge

--- misc/get_node_pool_size.py
+++ misc/get_node_pool_size.py
@@ -5,7 +5,7 @@ client = container_v1.ClusterManagerClient()
 ########################################################################################################################
 
 request = container_v1.GetNodePoolRequest()
-request.name = "projects/test-bed-fltk-kponichtera/locations/us-central1-c/clusters/fltk-testbed-cluster/nodePools/medium-fltk-pool-1"
+request.name = "projects/qpe2022project/locations/us-central1-c/clusters/fltk-testbed-cluster/nodePools/medium-fltk-pool-1"
 
 response = client.get_node_pool(request=request)
 
